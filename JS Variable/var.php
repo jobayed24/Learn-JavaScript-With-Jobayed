eef7749b5374770108c4713d604ccb96a38169da
jobayed24 <jkhouse117@gmail.com>
2021-09-13 21:01:21 +0600

    example js variable with html markup

---
+++ JS Variable/var.php
@@ -0,0 +1,15 @@
+<html>
+
+    <div id="demo">
+        <h1>Jobayed Hossen</h1>
+    </div>
+
+
+    <script>
+        var name;
+
+        name="jobayed hossen";
+       
+        document.getElementById("demo").innerHTML=name;
+    </script>
+</html>
