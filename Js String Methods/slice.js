5adf364f2d6d5b5dde79a7b8b8f56ad8a5eec00f
jobayed24 <jkhouse117@gmail.com>
2021-09-19 19:54:31 +0600

    array slice

---
+++ Js String Methods/slice.js
@@ -0,0 +1,12 @@
+// var fruit=["jobayed","hossen","Jonayed"];
+
+
+// var slicFruit= fruit.slice(0,2);
+
+// console.log(slicFruit);
+var fishPrice=5;
+var fishCount=6;
+
+var totalPrice=fishPrice*fishCount;
+
+console.log(totalPrice);
