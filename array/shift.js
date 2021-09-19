b333b801a2ba5e64bbf4d3a45eb50702419627fb
jobayed24 <jkhouse117@gmail.com>
2021-09-19 22:48:57 +0600

    shift

---
+++ array/shift.js
@@ -0,0 +1,13 @@
+
+//here is name printing as by while methods by untile when
+// run untill when "undefined" but till 
+
+var names = ["Andrew", "Edward", "Paul", "Chris" ,"John"];
+
+while(typeof(i=names.shift())!=="undefined"){
+    console.log(i);
+}
+
+
+//it's means undefined
+// console.log(typeof(names.shift()));
