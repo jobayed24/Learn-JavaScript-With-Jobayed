2acec8f3cbf64405c92e05b76f4e6d9fa446f095
jobayed24 <jkhouse117@gmail.com>
2021-09-20 23:44:48 +0600

    copywithin

---
+++ array/copyWIth.js
@@ -0,0 +1,17 @@
+// target means where actually replace start to end index
+
+
+///with copywithin array Function
+
+
+/// and it's make shallow copy means it's change the main array
+
+
+
+
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+
+
+const newarray=array1.copyWithin(2,4);
+
+console.log(array1);
