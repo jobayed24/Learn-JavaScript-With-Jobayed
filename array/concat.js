cbf30d2c00c75d89235debc2878b8d5fc99aa323
jobayed24 <jkhouse117@gmail.com>
2021-09-20 23:20:10 +0600

    concat

---
+++ array/concat.js
@@ -0,0 +1,15 @@
+
+// actually array concat like it's merge two arrays 
+// and return new array it's not modify base arrays
+
+///_----------------Finally i notice it's work in string---
+
+const data1=["a","b","c","d","e","f"];
+
+const data2=["g","h","i","j","k","l"];
+
+
+const newData=data1.concat(data2);
+
+
+console.log(newData);
